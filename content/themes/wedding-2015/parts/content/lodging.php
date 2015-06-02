d1085086862fbe88fc061d77d13f070a0e9690f1
Bruce Bentley <bbentley@mbopartners.com>
2015-06-02 11:02:47 -0400

    Added Lodging and Acommodation information

---
+++ content/themes/wedding-2015/parts/content/lodging.php
@@ -0,0 +1,55 @@
+<div class="row lodging__details">
+<h4 class="sub-title centered">Lodging &amp; Accommodations<br>
+<small>We've worked out a deal, so you can stay and enjoy the entire evening with us!</small>
+</h4>
+<hr class="divider love">
+<br>
+<div class="col-md-8 col-md-offset-2">
+<div class="welcome centered">
+<p>We've partnered with <a href="http://www.marriott.com/hotels/travel/wgots-towneplace-suites-winchester/" target="_blank">TownePlace Suites by Marriott Winchester</a>, so that our guests don't have to feel obligated to make the trip all the way home, especially after a long night on the dancefloor.</p>
+<p>Reservations for the event are recommendended to be made by <strong>June 11, 2015</strong>. All you need to do to capture the group-rate pricing, is mention:</p>
+<h3>Slupe-Bentley Wedding</h3>
+</div>
+<br>
+<div class="table-responsive">
+<table class="table">
+<caption><strong>GROUP ROOM RATES &amp; CONCESSIONS</strong></caption>
+<thead>
+<tr>
+<th>Room Type</th>
+<th>Rates</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Studio</td>
+<td>$75.00 / night</td>
+</tr>
+<tr>
+<td>One Bedroom Suite</td>
+<td>$109.00 / night</td>
+</tr>
+<tr>
+<td>Two Bedroom Suite</td>
+<td>$124.00 / night</td>
+</tr>
+</tbody>
+</table>
+</div>
+<br>
+<div class="centered no-margin top bottom">
+<div class="vcard">
+<strong>TownePlace Suites by Marriott Winchester</strong><br>
+<a href="https://www.google.com/maps/place/170+Getty+Ln,+Winchester,+VA+22602/@39.1888622,-78.1294383,17z/data=!3m1!4b1!4m2!3m1!1s0x89b5efeb30173f79:0x75ee33799e345e29" target="_blank">
+170 Getty Lane<br>
+Winchester, VA 22603
+</a>
+<br>
+<a href="tel:+15407222722" class="phone">(540) 722-2722</a>
+</div>
+</div>
+<br>
+<div class="centered no-margin top">
+<a href="https://www.google.com/maps/dir/16+Rosemont+Manor+Ln,+Berryville,+VA+22611/170+Getty+Ln,+Winchester,+VA+22602/@39.1732121,-78.098645,13z/data=!3m1!4b1!4m14!4m13!1m5!1m1!1s0x89b5e30ba55d857b:0xfa419dd29d61d3a6!2m2!1d-77.9931161!2d39.1568465!1m5!1m1!1s0x89b5efeb30173f79:0x75ee33799e345e29!2m2!1d-78.1294383!2d39.1888622!3e0" class="btn btn-success btn-md btn-raised" target="_blank">Directions From The Venue <i class="icon fa fa-angle-right fa-fw"></i></a>
+</div>
+</div>
