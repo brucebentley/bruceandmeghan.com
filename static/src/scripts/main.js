7bff53c3c41ef97d264f00fbbbee77ecd6ea3373
Bruce Bentley <bbentley@mbopartners.com>
2015-06-12 16:43:52 -0400

    Removing unneccessary scripts from main.js and minifying everything again.

--- static/src/scripts/main.js
+++ static/src/scripts/main.js
@@ -12,14 +12,6 @@
     $(function() {
         // The DOM is ready!
 
-        //$('#our-story').prepend('<div class="section-background"><div class="section-background-image"></div></div>');
-
-        // Active Navigation Tab
-        $('.navbar-nav > li > a').click(function() {
-            $(this).parent().addClass('active');
-            $(this).parent().siblings().removeClass('active');
-        });
-
         // The Final Countdown
         $('#countdownRow').countdown('2015/07/11 17:30:00', function(event) {
             $(this).html(event.strftime('<div class="col-xs-6 col-sm-3 countdown-days"><div class="countdown-section"><span class="countdown-amount">%-D</span><span class="countdown-period">Day%!D</span></div></div><div class="col-xs-6 col-sm-3 countdown-hours"><div class="countdown-section"><span class="countdown-amount">%H</span><span class="countdown-period">Hour%!H</span></div></div><div class="col-xs-6 col-sm-3 countdown-minutes"><div class="countdown-section"><span class="countdown-amount">%M</span><span class="countdown-period">Minute%!M</span></div></div><div class="col-xs-6 col-sm-3 countdown-seconds"><div class="countdown-section"><span class="countdown-amount">%S</span><span class="countdown-period">Second%!S</span></div></div> '
@@ -36,56 +28,6 @@
             slide: '.slide',
             slidesToShow: 1,
             slidesToScroll: 1
-        });
-
-        $('#rsvpForm').on('submit', function(e) {
-            e.preventDefault();
-
-            var data = {
-                action : 'wufoo_post',
-                fields : $(this).serialize()
-            };
-
-            $.ajax({
-                type     : 'POST',
-                url      : '/wp-admin/admin-ajax.php',
-                data     : data,
-                dataType : 'json',
-                success  : function(data) {
-                    console.log(data);
-                    swal({
-                        title: 'RSVP Confirmed!',
-                        text: "We can't wait to see you there!",
-                        type: 'success',
-                        showCancelButton: 'true',
-                        confirmButtonColor: '#4db6ac',
-                        confirmButtonText: 'Nope, All Set!',
-                        cancelButtonColor: '#4db6ac',
-                        cancelButtonText: 'RSVP Another?',
-                        closeOnConfirm: false,
-                        closeOnCancel: true
-                    }, function() {
-                        swal({
-                            title: 'Thanks!',
-                            text: 'Your RSVP has been confirmed.',
-                            type: 'success',
-                            confirmButtonColor: '#4db6ac',
-                            confirmButtonText: 'Back To The Site',
-                            closeOnConfirm: true
-                        });
-                    });
-                },
-                error : function(data) {
-                    swal({
-                        title: 'Uh Oh!',
-                        text: "It looks like something didn't go quite as planned. We're sorry for that!",
-                        type: 'error',
-                        confirmButtonColor: '#e84e40',
-                        confirmButtonText: 'Try Again...',
-                        closeOnConfirm: true
-                    });
-                }
-            });
         });
     });
 
